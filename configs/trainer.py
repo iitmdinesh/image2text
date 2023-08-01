5d5fdb36a21de4580755730363cc5211d7e539bd
Dinesh Ramasamy <iitm.dinesh@gmail.com>
2023-07-31 21:42:40 -0700

    update config

--- configs/trainer.py
+++ configs/trainer.py
@@ -16,7 +16,7 @@ class TrainerWrapperConfig(BaseModel):
 
 
 class OptimizerConfig(BaseModel):
-    lr: float = 6e-4
+    lr: float
     weight_decay: float = 0.0
     target_modules: Optional[List[str]] = None
 
@@ -25,15 +25,14 @@ class TrainingConfig(BaseModel):
     model: VisionEncoderDecoderConfig
     disable_flash: bool = False
     ignore_index: int = -100
-    batch_size: int = 8
+    batch_size: int
     dataloader_buffer_size: int = 5
     shuffle: bool = True
-    gradient_accumulation_steps: int = 4
-    epochs: int = 6
+    gradient_accumulation_steps: int = 1
+    epochs: int = 1
     num_steps: Optional[int] = None
     num_val_steps: Optional[int] = None
     precision: str = 'no'
-    add_eos: bool = True
     tokenizer_str: str
     reset_moco_after_k_epochs: Optional[List[int]] = None
     trainer: TrainerWrapperConfig
